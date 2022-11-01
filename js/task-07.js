51d5d872666af16a92c11e2958507a74f686e89b
Olga Pikulska <olga.pikulska.ld@gmail.com>
2022-11-01 21:13:49 +0100

    fixed 2.0

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const range = document.querySelector("#font-size-control");
 const text = document.querySelector("#text");
-
+text.style.fontSize = "56px";
 const changeFontSize = () => {
   text.style.fontSize = range.value + "px";
 };
